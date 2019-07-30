9d6512d8d2bce4658a075c4e5ac4cbcf94a2ab7d
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-30 10:16:19 +0200

    fase5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const myWordList =  [
+    {
+      text: 'Pencil',
+      total: 6
+    },
+    {
+      text: 'Thermo',
+      total: 2
+    },
+    {
+      text: 'TV',
+      total: 8
+    },
+    {
+      text: 'Phone',
+      total: 4
+    }
+  ];
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
@@ -10,6 +29,11 @@ function writeThis (str, maxNumber) {
     }
 }
 
-writeThis ('Patata', getRandomNumber(10));
-writeThis ('Aguacate', getRandomNumber(30));
-writeThis ('Pizza', getRandomNumber(50));
+function writeMyArray () {
+    for (const item of myWordList) {
+        //console.log(item.text, item.total);
+        writeThis(item.text, item.total);
+    }
+}
+
+writeMyArray ();
