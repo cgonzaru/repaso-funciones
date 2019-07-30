2ae89162889cfc4a4bb34b38137fa420586081dc
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-30 10:02:30 +0200

    fase 4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 'use strict';
 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
 function writeThis (str, maxNumber) {
     for (let i=0; i<maxNumber; i++) {
         console.log(str);
     }
 }
 
-writeThis ('Patata', 10);
-writeThis ('Aguacate', 7);
-writeThis ('Pizza', 50);
+writeThis ('Patata', getRandomNumber(10));
+writeThis ('Aguacate', getRandomNumber(30));
+writeThis ('Pizza', getRandomNumber(50));
